b7eaf793bb77abd7cd6509742b64212302b6dcc2
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-04-15 10:40:36 +0530

    Final project with animation

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Die = (props) => {
     return (
-        <div>
-            <i className={"Die-Dice fas fa-dice-" + props.number + " fa-7x"} />
+        <div >
+            <i className={`Die-Dice fas fa-dice-${props.number} fa-7x ${props.rolling && "shaking"}`} />
         </div >
     )
 }
--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -4,19 +4,25 @@ import { Die } from './Die';
 export const RollDice = () => {
 
     const [dice, setDice] = useState({ diceOne: "one", diceTwo: "one" })
+    const [rolling, setRolling] = useState(false)
 
     const diceArray = ["one", "two", "three", "four", "five", "six"];
 
     const roll = () => diceArray[Math.floor(Math.random() * diceArray.length)];
 
-    const handleClick = e => setDice({ diceOne: roll(), diceTwo: roll() })
+    const handleClick = e => { setDice({ diceOne: roll(), diceTwo: roll() }); setRolling(true) }
+
+    setTimeout(() => setRolling(false), 1000);
 
 
     return (
-        <div>
-            <Die number={dice.diceOne} />
-            <Die number={dice.diceTwo} />
-            <button onClick={() => handleClick()}>Roll</button>
+        <div className="Roll-Dice">
+            <div className="Roll-Dice-container">
+                <Die rolling={rolling} number={dice.diceOne} />
+                <Die rolling={rolling} number={dice.diceTwo} />
+            </div>
+
+            <button disabled={rolling} className="Die-button" onClick={() => handleClick()}>{rolling ? "Rolling" : "Roll Dice!"}</button>
         </div>
     )
 }
